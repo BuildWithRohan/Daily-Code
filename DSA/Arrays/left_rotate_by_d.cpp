0bac820ca4363da2099a2922fe8251eb28f2be5b
BuildWithRohan <rohankumarsmi@gmail.com>
2026-01-15 13:07:28 +0530

    Striver Sheet: Left rotate array by D positions

---
+++ DSA/Arrays/left_rotate_by_d.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n, d;
+    cin >> n >> d; //Input n and d
+
+    int arr[n];
+    // Input Array
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    d = d % n;
+
+    reverse(arr, 0, d - 1);
+    reverse(arr, d, n - 1);
+    reverse(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
